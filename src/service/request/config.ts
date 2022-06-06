b9db5894696c91bd944a20383ae60858ed1d03cf
charhua <trk12138@163.com>
2022-06-06 13:19:21 +0800

    feat: fix a host bug

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,8 +4,8 @@ const TIME_OUT = 5000;
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api';
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://123.207.32.32:8000/';
+  BASE_URL = 'http://152.136.185.210:5000/';
 } else {
-  BASE_URL = 'http://123.207.32.32:8000/';
+  BASE_URL = 'http://152.136.185.210:5000/';
 }
 export { BASE_URL, TIME_OUT };
